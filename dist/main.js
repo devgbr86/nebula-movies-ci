517cff85a2812a853494cdaab05fa08505ee9ba3
devgbr86 <devgbr86@users.noreply.github.com>
2026-04-01 19:26:30 +0000

    deploy: 5270c6a57be04185088c811c3d7a5b1bd52a4119

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+import { initRouter, register } from "./core/router.js";
+import { renderHome } from "./pages/home.js";
+import { renderSearch } from "./pages/search.js";
+const app = document.getElementById("app");
+register("/", () => renderHome(app));
+register("/search", () => renderSearch(app));
+initRouter();
